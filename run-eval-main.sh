05d4c008ecaf7e6f587ca5e16d93395ed4c3f004
Alexis Gobé <alexis.gobe@inria.fr>
2025-02-24 14:46:25 +0100

    Add TMP_DIR env var

--- run-eval-main.sh
+++ run-eval-main.sh
@@ -7,11 +7,13 @@ apptainer exec --no-mount home --nv --bind $PWD --bind /var/lib/oar  --bind ~/.h
 echo "inside container"
 echo $PWD
 
-export HF_HOME=/tmp/${USER}-runtime-dir/cache
+export TMP_DIR=/tmp/${USER}-runtime-dir
+mkdir -p ${TMP_DIR}
+export HF_HOME=${TMP_DIR}/cache
 mkdir -p ${HF_HOME}
-export TRITON_CACHE_DIR=/tmp/${USER}-runtime-dir/triton
+export TRITON_CACHE_DIR=${TMP_DIR}/triton
 mkdir -p ${TRITON_CACHE_DIR}
-export VLLM_CONFIG_ROOT=/tmp/${USER}-runtime-dir/.config/vllm
+export VLLM_CONFIG_ROOT=${TMP_DIR}/.config/vllm
 mkdir -p ${VLLM_CONFIG_ROOT}
 
 HF_TOKEN=$(cat ~/.hf_token)
@@ -24,8 +26,8 @@ NODES=$(cat ${OAR_FILE_NODES} | uniq | grep -v ${HOSTNAME})
 echo "Other nodes: ${NODES}"
 for host in $NODES;
 do
-  scp run-eval-workers.sh $host:/tmp/${USER}/run-eval-workers.sh
-  ssh $host /tmp/${USER}:run-eval-workers.sh $(hostname -I | cut -d " " -f1)
+  scp run-eval-workers.sh $host:${TMP_DIR}/run-eval-workers.sh
+  ssh $host ${TMP_DIR}/run-eval-workers.sh $(hostname -I | cut -d " " -f1)
 done
 
 ray status
