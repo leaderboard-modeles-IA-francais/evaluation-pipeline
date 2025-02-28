a5644b087a637879b8c60458985e4ebe11195cd4
LE BRETON Come <come.le-breton@inria.fr>
2025-02-28 10:53:23 +0100

    update

--- run-eval-main.py
+++ run-eval-main.py
@@ -2,7 +2,9 @@ import subprocess
 import os
 
 def main():
-    subprocess.run("run-eval-main.sh", shell=True, executable="/bin/bash")
+    subprocess.run("pwd", shell=True, executable="/bin/bash")
+    subprocess.run("ls", shell=True, executable="/bin/bash")
+    subprocess.run("./run-eval-main.sh", shell=True, executable="/bin/bash")
 
 if __name__ == "__main__":
     main()
