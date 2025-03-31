3aaed9c772a58ce9c4780ea2d79300676900c9e8
Alexis Gobé <alexis.gobe@inria.fr>
2025-03-31 15:01:15 +0200

    Add: Interactive scripts for debugging/testing/benchmarking stuff

---
+++ run-eval-main-interactive.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+module load conda
+conda activate py3.12
+
+echo "inside conda env"
+echo $PWD
+echo $CONDA_PREFIX
+
+export TMP_DIR=/tmp/${USER}-runtime-dir
+mkdir -p ${TMP_DIR}
+export HF_HOME=${TMP_DIR}/cache
+mkdir -p ${HF_HOME}
+export TRITON_CACHE_DIR=${TMP_DIR}/triton
+mkdir -p ${TRITON_CACHE_DIR}
+export VLLM_CONFIG_ROOT=${TMP_DIR}/.config/vllm
+mkdir -p ${VLLM_CONFIG_ROOT}
+export OUTPUT_DIR=$TMP_DIR/results
+export DETAIL_DIR=$OUTPUT_DIR/details
+export RESULT_DIR=$OUTPUT_DIR/clearML-musa
+
+HF_TOKEN=$(cat ~/.hf_token)
+
+huggingface-cli login --token ${HF_TOKEN}
+
+NODES=($(cat ${OAR_FILE_NODES} | uniq | grep -v ${HOSTNAME}))
+NNODES=${#NODES[@]}
+echo "Number of nodes: $(($NNODES+1))"
+echo "Current node: ${HOSTNAME}"
+
+if (($NNODES>0)); then
+   ray start --head --port=6379
+   for ((i=0; i<${NNODES}; i++));
+   do
+	   echo "Other nodes: Index $i - Node ${NODES[i]}"
+	   ssh ${NODES[i]} "bash -s" < run-eval-workers.sh $(hostname -I | cut -d " " -f1)
+   done
+   ray status
+else
+   echo "Mononoeud so declare envvar"
+   # export VLLM_WORKER_MULTIPROC_METHOD=spawn
+   # export VLLM_SKIP_P2P_CHECK=1
+fi
+
+# pip list
+
+python3 run-lighteval-interactive.py
+rm -rf $DETAIL_DIR
+mv $OUTPUT_DIR/results $RESULT_DIR
+
+export HF_USER_ACCESS_GIT=$(cat ~/.hf_push_user)
+export HF_TOKEN_ACCESS_GIT=$(cat ~/.hf_push_token)
+python3 push_results.py $RESULT_DIR
---
+++ run-lighteval-interactive.py
@@ -0,0 +1,76 @@
+import os
+from pathlib import Path
+# from clearml import Task
+from lighteval.logging.evaluation_tracker import EvaluationTracker
+from lighteval.models.vllm.vllm_model import VLLMModelConfig
+from lighteval.pipeline import ParallelismManager, Pipeline, PipelineParameters
+from lighteval.utils.utils import EnvConfig
+
+unix_user=os.environ.get("USER")
+output_dir=f"/tmp/{unix_user}-runtime-dir/results"
+tasks_path=Path(f"tasks/french_evals.py")
+
+# FIXME get task by id instead
+# task = Task.init(project_name = "LLM Leaderboard FR", task_name = "eval_model")
+
+# FIXME
+# vllm_model.py:211: FutureWarning: It is strongly recommended to run mistral models with `--tokenizer-mode "mistral"` to ensure correct encoding and decoding.
+
+parameters = {
+    'model': 'meta-llama/Llama-3.2-3B-Instruct',
+    'dtype': 'bfloat16',
+    'gpu_memory_utilization': 0.5,
+    'tensor_parallel_size': 2,
+    'enforce_eager': True,
+    'tasks': 'community|bac-fr|0|0,community|ifeval-fr|0|0,community|pr-fouras|0|0,community|gpqa-fr|0|0',
+    'max_model_length': 8192,
+    'use_chat_template': True,
+}
+
+# task.connect(parameters)
+
+evaluation_tracker = EvaluationTracker(
+    output_dir=output_dir,
+    save_details=True,
+    push_to_hub=False,
+)
+
+pipeline_params = PipelineParameters(
+    launcher_type=ParallelismManager.VLLM,
+    custom_tasks_directory=tasks_path,
+    use_chat_template=parameters['use_chat_template'],
+    #env_config=EnvConfig(cache_dir="tmp/"),
+    # Remove the 2 parameters below once your configuration is tested
+    #override_batch_size=1,
+    #max_samples=10
+)
+
+model_config = VLLMModelConfig(
+    pretrained=parameters['model'],
+    dtype=parameters['dtype'],
+    gpu_memory_utilization=parameters['gpu_memory_utilization'],
+    tensor_parallel_size=parameters['tensor_parallel_size'],
+    enforce_eager=parameters['enforce_eager'],
+    #max_model_length=parameters['max_model_length'],
+    use_chat_template=parameters['use_chat_template'],
+)
+
+pipeline = Pipeline(
+    tasks=parameters["tasks"],
+    pipeline_parameters=pipeline_params,
+    evaluation_tracker=evaluation_tracker,
+    model_config=model_config,
+)
+
+pipeline.evaluate()
+pipeline.save_and_push_results()
+pipeline.show_results()
+
+final = pipeline.get_results()
+# logger = task.get_logger()
+print(final)
+#for task_name, metrics in final['results'].items():
+#    for k, v in metrics.items():
+#        logger.report_single_value(f"{task_name} | {k}", v)
+
+# task.upload_artifact(name='results', artifact_object=output_dir)
