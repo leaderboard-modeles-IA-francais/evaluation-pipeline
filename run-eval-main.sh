a62a063009ca788f29a8c736dbb54dcf4ac0a9d6
LE BRETON Come <come.le-breton@inria.fr>
2025-04-24 14:04:23 +0200

    push details

--- run-eval-main.sh
+++ run-eval-main.sh
@@ -46,13 +46,15 @@ pip list
 #NGPUS=$(($NGPUSPERNODES*($NNODES+1)))
 
 python3 run-lighteval.py
-rm -rf $DETAIL_DIR
 if [ -d "$OUTPUT_DIR/results" ]; then
   mv $OUTPUT_DIR/results $RESULT_DIR
 
   export HF_USER_ACCESS_GIT=$(cat ~/.hf_push_user)
   export HF_TOKEN_ACCESS_GIT=$(cat ~/.hf_push_token)
-  python3 push_results.py $RESULT_DIR
+  python3 push_results.py $DETAIL_DIR "details-dev"
+  rm -rf $DETAIL_DIR
+  python3 push_results.py $RESULT_DIR "results-dev"
+
 else
   echo "No $OUTPUT_DIR/results directory, error"
   exit 1
